4e6f0d00e91acb65c10bcf4c1a957a0e097ae9be
Oleksiy Dubovyk <oleksiy_dubovyk@epam.com>
2017-09-29 00:42:00 +0300

    enzyme config

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{js,jsx}'],
   setupFiles: [
     'raf/polyfill',
-    '<rootDir>/jest.setup.js',
+    '<rootDir>/enzyme.config.js',
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/__mocks__/fileMock.js',
